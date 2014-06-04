a995e1079cc787754ea2ed7168dff66a06edaecc
Brent Kastner <brentkastner@gmail.com>
2014-06-04 09:35:05 -0500

    testing local auth

--- lib/omniauth/strategies/acclaim.rb
+++ lib/omniauth/strategies/acclaim.rb
@@ -11,6 +11,27 @@ module OmniAuth
           :access_token_path => '/oauth/token',
           :authorize_url => 'https://localhost:4000/oauth/authorize'
       }
+
+      uid { raw_info['id'] }
+
+      info do
+        {
+            :name => raw_info['name'],
+            :email => raw_info['email']
+        }
+      end
+
+      extra do
+        {
+            'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/oauth/me.json').parsed
+      end
+
+
     end
   end
 end
